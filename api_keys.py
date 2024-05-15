f7e96ebdb463f5c07d74ae7c740280ff14cb12bf
Ariane Wilhelm <arianedwilhelm@gmail.com>
2024-05-15 13:38:13 +0200

    changed a comment

---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# groq key
+api_key_groq = "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"
+
+# elsevier key for ScienceDirect
+api_key_elsevier = "1bafda725082a9d87b9b160d375fe35c"
---
+++ test_please_delete.py
@@ -0,0 +1,5 @@
+print('Hello')
+x = 2
+y = 3
+z = x + y
+print(z)
